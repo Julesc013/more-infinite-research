c47fb97e248de7f8ccde959433d19db04713577f
Jules Carboni <30209022+Julesc013@users.noreply.github.com>
2026-07-14 07:40:41 +1000

    release(0.8): materialize finite museum candidate

---
+++ config.lua
@@ -0,0 +1,9 @@
+-- More Infinite Research museum configuration.
+-- This file is loaded by data.lua. Values above the compiled hard limits are clamped.
+mir_config = {
+  ["inserter-capacity"] = { enabled = true, levels = 5 },
+  ["gun-turret-damage"] = { enabled = true, levels = 5 },
+  ["bullet-shooting-speed"] = { enabled = true, levels = 5 },
+  ["bullet-damage"] = { enabled = true, levels = 5 },
+  ["toolbelt"] = { enabled = true, levels = 3 }
+}
